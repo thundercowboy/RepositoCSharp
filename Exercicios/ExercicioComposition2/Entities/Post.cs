67c57a66657eaa362b4ee3ed01ed1252c0db5ea3
Raphael Pedroso <raphael.pedroso97@hotmail.com>
2024-04-24 13:45:38 -0300

    Adicionado exercicio 2, exercicio 1 estruturado

---
+++ Exercicios/ExercicioComposition2/Entities/Post.cs
@@ -0,0 +1,55 @@
+
+using System.Text;
+using System;
+using System.Collections.Generic;
+
+
+namespace programacs.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+        public Post()
+        {
+        }
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+        public override string ToString()
+        {   
+            // a classe post podem possuir inumeros comentarios
+            // e concatenar manualmente irá demorar e degradar a performance.
+            // Logo, usamos a classe StringBuilder para montar uma string
+            // conforme solicitado de uma forma mais eficiente e devolve
+            // o resultado em string.
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Likes - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments:");
+            foreach (Comment c in Comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+            return sb.ToString();
+        }
+    }
+}
