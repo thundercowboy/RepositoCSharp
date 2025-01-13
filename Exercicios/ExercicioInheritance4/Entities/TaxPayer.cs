9b2184bcf9327b3461b3c43d26df2e4c02955c67
raphaelpedroso <raphael.pedroso97@hotmail.com>
2025-01-13 11:05:14 -0300

    Add: ExercicioInheritance 4

---
+++ Exercicios/ExercicioInheritance4/Entities/TaxPayer.cs
@@ -0,0 +1,16 @@
+
+namespace Main.Entities
+{
+    abstract class TaxPayer
+    {
+        public string Name { get; set; }
+        public double AnualIncome { get; set; }
+        public TaxPayer() {
+        }
+        protected TaxPayer(string name, double anualIncome) {
+            Name = name;
+            AnualIncome = anualIncome;
+        }
+        public abstract double Tax();
+    }
+}
