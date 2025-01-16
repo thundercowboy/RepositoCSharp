b23eaa627a69d167d6da4e8a309ba550d8099c94
raphaelpedroso <raphael.pedroso97@hotmail.com>
2025-01-16 13:55:03 -0300

    Add: Exercicio Lambda 4

---
+++ Exercicios/ExercicioLambda4/Entities/Product.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Main.Entities
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
